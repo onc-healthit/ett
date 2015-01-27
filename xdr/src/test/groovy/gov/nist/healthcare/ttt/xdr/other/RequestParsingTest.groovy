dc7a3f03cb2004105f161b5a9eedcfee0cb2178f
antoine gerardin <antoine.gerardin@gmail.com>
2015-01-27 17:30:01 -0500

    fix request parsing

--- xdr/src/test/groovy/gov/nist/healthcare/ttt/xdr/other/RequestParsingTest.groovy
+++ xdr/src/test/groovy/gov/nist/healthcare/ttt/xdr/other/RequestParsingTest.groovy
@@ -17,11 +17,14 @@ class RequestParsingTest extends Specification {
 
         given:
 
-        def file = this.getClass().getClassLoader().getResourceAsStream("xdr_full_metadata_sample_request_escaped_FOR_REAL.txt")
+        def stream = this.getClass().getClassLoader().getResourceAsStream("xdr_full_metadata_sample_request_escaped.txt")
+        String string = org.apache.commons.io.IOUtils.toString(stream, "UTF-8");
+        String unescapedString = StringEscapeUtils.unescapeXml(string)
+        InputStream unescapedStream = new ByteArrayInputStream(unescapedString.getBytes())
 
         when:
 
-        MimeMessage msg = new MimeMessage(Session.getDefaultInstance(new Properties()), file)
+        MimeMessage msg = new MimeMessage(Session.getDefaultInstance(new Properties()), unescapedStream)
         Multipart content = msg.getContent()
         MimeBodyPart part1 = content.getBodyPart(0)
 
@@ -30,20 +33,13 @@ class RequestParsingTest extends Specification {
         println out.toString()
 
         String xml = org.apache.commons.io.IOUtils.toString(part1.getInputStream(), "UTF-8");
+        println xml
 
-        String processed = StringEscapeUtils.unescapeXml(xml)
-
-        println processed
-
-        def envelope = new XmlSlurper().parseText(processed)
-
-
-
+        def envelope = new XmlSlurper().parseText(xml)
         def directFrom = envelope.Header.addressBlock.from.text()
 
-        println directFrom
-
         then:
+        println directFrom
         assert directFrom == "directFrom"
 
 
@@ -53,12 +49,19 @@ class RequestParsingTest extends Specification {
     def testRequestParsingWithEscapedXmlAndHttpHeadersFrom2Files() {
 
         given:
-
         def file1 = this.getClass().getClassLoader().getResourceAsStream("new/header.txt")
         def file2 = this.getClass().getClassLoader().getResourceAsStream("new/body.txt")
-        when:
 
-        def file3 = new java.io.SequenceInputStream(file1, file2)
+        when:
+        String string1 = org.apache.commons.io.IOUtils.toString(file1, "UTF-8");
+        String unescapedString1 = StringEscapeUtils.unescapeXml(string1)
+        InputStream unescapedStream1 = new ByteArrayInputStream(unescapedString1.getBytes())
+
+        String string2 = org.apache.commons.io.IOUtils.toString(file2, "UTF-8");
+        String unescapedString2 = StringEscapeUtils.unescapeXml(string2)
+        InputStream unescapedStream2 = new ByteArrayInputStream(unescapedString2.getBytes())
+
+        def file3 = new java.io.SequenceInputStream(unescapedStream1, unescapedStream2)
 
         MimeMessage msg = new MimeMessage(Session.getDefaultInstance(new Properties()), file3)
         Multipart content = msg.getContent()
@@ -70,14 +73,8 @@ class RequestParsingTest extends Specification {
 
         String xml = org.apache.commons.io.IOUtils.toString(part1.getInputStream(), "UTF-8");
 
-        String processed = StringEscapeUtils.unescapeXml(xml)
 
-        println processed
-
-        def envelope = new XmlSlurper().parseText(processed)
-
-
-
+        def envelope = new XmlSlurper().parseText(xml)
         def directFrom = envelope.Header.addressBlock.from.text()
 
         println directFrom
@@ -100,8 +97,10 @@ class RequestParsingTest extends Specification {
 
         String header = org.apache.commons.io.IOUtils.toString(file1, "UTF-8");
         String body = org.apache.commons.io.IOUtils.toString(file2, "UTF-8");
+        String unescapedString1 = StringEscapeUtils.unescapeXml(header)
+        String unescapedString2 = StringEscapeUtils.unescapeXml(body)
 
-        def request = header + body
+        def request = unescapedString1 + unescapedString2
 
         InputStream req = new ByteArrayInputStream(request.getBytes());
 
@@ -115,14 +114,7 @@ class RequestParsingTest extends Specification {
 
         String xml = org.apache.commons.io.IOUtils.toString(part1.getInputStream(), "UTF-8");
 
-        String processed = StringEscapeUtils.unescapeXml(xml)
-
-        println processed
-
-        def envelope = new XmlSlurper().parseText(processed)
-
-
-
+        def envelope = new XmlSlurper().parseText(xml)
         def directFrom = envelope.Header.addressBlock.from.text()
 
         println directFrom
