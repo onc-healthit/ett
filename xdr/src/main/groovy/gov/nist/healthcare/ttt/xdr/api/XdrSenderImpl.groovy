cc5e13218cca1bc55695f88cf2fee5c6e0864ae6
julienperugini <unknown>
2016-03-16 16:04:01 -0400

    Start work for new toolkit

--- xdr/src/main/groovy/gov/nist/healthcare/ttt/xdr/api/XdrSenderImpl.groovy
+++ xdr/src/main/groovy/gov/nist/healthcare/ttt/xdr/api/XdrSenderImpl.groovy
@@ -3,6 +3,12 @@ import gov.nist.healthcare.ttt.tempxdrcommunication.artifact.ArtifactManagement
 import gov.nist.healthcare.ttt.tempxdrcommunication.artifact.Artifacts
 import gov.nist.healthcare.ttt.tempxdrcommunication.artifact.Settings
 import gov.nist.healthcare.ttt.xdr.web.GroovyRestClient
+import gov.nist.toolkit.toolkitApi.BasicSimParameters
+import gov.nist.toolkit.toolkitApi.DocumentSource
+import gov.nist.toolkit.toolkitApi.SimulatorBuilder
+import gov.nist.toolkit.toolkitServicesCommon.RawSendResponse
+import gov.nist.toolkit.toolkitServicesCommon.SimConfig
+import gov.nist.toolkit.toolkitServicesCommon.resource.DocumentResource
 import groovy.util.slurpersupport.GPathResult
 import groovy.xml.XmlUtil
 import org.apache.commons.io.IOUtils
@@ -85,7 +91,51 @@ class XdrSenderImpl implements XdrSender{
         catch(groovyx.net.http.ResponseParseException e){
             throw new RuntimeException("could not understand response from toolkit.",e)
         }
-    }
+		
+		String urlRoot = String.format("http://localhost:8080/xdstools2");
+
+		SimulatorBuilder spi = new SimulatorBuilder(urlRoot);
+		BasicSimParameters srcParams = new BasicSimParameters();
+
+		srcParams.setId("source");
+		srcParams.setUser("bill");
+		srcParams.setActorType(SimulatorActorType.DOCUMENT_SOURCE);
+		srcParams.setEnvironmentName("test");
+
+		System.out.println("STEP - CREATE DOCSRC SIM");
+		DocumentSource documentSource = spi.createDocumentSource(
+		srcParams.getId(),
+		srcParams.getUser(),
+		srcParams.getEnvironmentName()
+		);
+
+
+		System.out.println("verify sim built");
+		System.out.println(documentSource.getId() == srcParams.getId());
+
+		System.out.println("STEP - UPDATE - SET DOC REC ENDPOINTS INTO DOC SRC");
+		//		documentSource.setProperty(SimulatorProperties.pnrEndpoint, documentRecipient.asString(SimulatorProperties.pnrEndpoint));
+		//		documentSource.setProperty(SimulatorProperties.pnrEndpoint, "http://hit-dev.nist.gov:11080/xdstools3/sim/ett/10/docrec/prb");
+		if(config.targetEnpoitTLS.startsWith("https")) {
+			documentSource.setProperty(SimulatorProperties.pnrTLSEndpoint, config.targetEnpoitTLS);
+		} else {
+		documentSource.setProperty(SimulatorProperties.pnrEndpoint, config.targetEnpoitTLS);
+		}
+		SimConfig updatedVersion = documentSource.update(documentSource.getConfig());
+		System.out.println("Updated Src Sim config is " + updatedVersion.describe());
+
+		System.out.println(updatedVersion);
+		
+		InputStream ccdaAttachment = new URL(config.payload.link).openStream();
+		req.setMetadata(IOUtils.toString(ccdaAttachment));
+		DocumentResource document = new DocumentResource();
+		document.setContents("Hello World!".getBytes());
+		document.setMimeType("text/plain");
+		req.addDocument("Document01", document);
+
+		RawSendResponse response = documentSource.sendProvideAndRegister(req);
+		
+	}
 
     private def parseSendXdrResponse(GPathResult r){
         def report = [:]
