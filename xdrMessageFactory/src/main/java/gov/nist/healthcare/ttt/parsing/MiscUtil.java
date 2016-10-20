fe6464bfe015f8a1de4aa3ef66dd6663c993b874
sriniadhiwork <sriniadhi.work@gmail.com>
2016-10-20 17:38:05 -0400

    adding missing imports

--- xdrMessageFactory/src/main/java/gov/nist/healthcare/ttt/parsing/MiscUtil.java
+++ xdrMessageFactory/src/main/java/gov/nist/healthcare/ttt/parsing/MiscUtil.java
@@ -6,17 +6,24 @@
 package gov.nist.healthcare.ttt.parsing;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.io.StringWriter;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 /**
  *
@@ -52,6 +59,7 @@ public class MiscUtil {
         DocumentBuilder builder = factory.newDocumentBuilder();
         return builder.parse(new InputSource(new StringReader(xmlSource)));
     }
+
     
     static String readFile(String path, Charset encoding) throws IOException {
         byte[] encoded = Files.readAllBytes(Paths.get(path));
