4c2068aca374a2b11c13d207772e7f8ce2b1705e
svalluripalli <sai@digitalhie.com>
2022-12-19 07:32:09 -0500

    SITE-3502: Various signature algos and encryption/ decryption are supported in this checkin

--- webapp/src/main/groovy/gov/nist/healthcare/ttt/webapp/direct/controller/SendDirectController.groovy
+++ webapp/src/main/groovy/gov/nist/healthcare/ttt/webapp/direct/controller/SendDirectController.groovy
@@ -69,12 +69,20 @@ public class SendDirectController {
 		}
 
 		if(messageInfo.getDigestAlgo().equals("edsasha256")) {
-			digestAlgo = "SHA256withECDSA";
+			digestAlgo = "SHA256withPLAIN-ECDSA";
 		}
 
 		if(messageInfo.getDigestAlgo().equals("edsasha384")) {
-			digestAlgo = "SHA384withECDSA";
+			digestAlgo = "SHA384withPLAIN-ECDSA";
 		}
+                
+                if(messageInfo.getDigestAlgo().equals("edsap256")) {
+                        digestAlgo = "SHA256withECDSA";
+                }
+                
+                if(messageInfo.getDigestAlgo().equals("edsap384")) {
+                        digestAlgo = "SHA384withECDSA";
+                }
 
 		if (messageInfo.isValidSendEmail()) {
 			InputStream attachmentFile = null;
