c464d94c4d8abb9084c24e9d1b1fe895f8fe94a1
Girish <bgirishrao@drajer.com>
2023-03-20 11:46:54 -0400

    SITE-3561

--- gui/src/app/direct/cert-discovery/dcdt.js
+++ gui/src/app/direct/cert-discovery/dcdt.js
@@ -2,7 +2,7 @@ var dcdtValidator = angular.module('ttt.direct.dcdtValidator', []);
 
 dcdtValidator.controller('DCDTValidatorCtrl', ['$scope', 'DCDTValidatorFactory', '$state', 'ApiUrl','$http','CCDADocumentsFactory','$timeout', 'growl','$sce','$location', '$anchorScroll',
 	function($scope, DCDTValidatorFactory, $state, ApiUrl,$http,CCDADocumentsFactory,$timeout ,growl,$sce,$location,$anchorScroll) {
-	$scope.pageTitle= $state.current.data.pageTitle;
+	$scope.pageTitle= "2015";
 	$scope.year2015 = ($scope.pageTitle === "2015");
     if (!$scope.year2015 ){
         $scope.year2015 = ($scope.pageTitle === "SVAP 2022");
