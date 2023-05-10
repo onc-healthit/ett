32d6657cb48a86b918ccc70995025495a3c21efd
ebrockainq <ebrock@ainq.com>
2023-05-10 09:35:27 -0700

    Update error codes
    Update error codes for file size issues, also updated message for R2 validator

--- webapp/src/main/java/gov/nist/healthcare/ttt/webapp/common/controller/SVAPValidatorCtrl.java
+++ webapp/src/main/java/gov/nist/healthcare/ttt/webapp/common/controller/SVAPValidatorCtrl.java
@@ -62,7 +62,7 @@ public class SVAPValidatorCtrl {
 				String fileName = normalizedPath.toString();
 				File file = new File(fileName);
 				if(TempUploadController.isValidFileSize()) {
-					throw new TTTCustomException("0x0050", "Attached files cannot be larger than 1MB");
+					throw new TTTCustomException("0x0070", "Attached files cannot be larger than 1MB");
 				}
 				if(!file.exists() || fileName.startsWith("../") || !fileName.startsWith(tDir + File.separator)) {
 					throw new TTTCustomException("0x0050", "Action not supported by API.");
